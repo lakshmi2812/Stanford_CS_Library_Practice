29935177d94d5854c66bf1da17b7a83c808307dd
lakshmi2812 <laksmi.nishthala@gmail.com>
2019-09-10 11:11:05 -0700

    size() seems to work fine

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -39,7 +39,9 @@ public class BinarySearchTree{
     }
     
     
-    //Binary Tree Problems
+    //********/ Binary Tree Problems /**********/
+
+    //build123() -> Build a linkedlist with nodes 1,2,3
     public void build123(){
         if(this.root == null){
             this.root = new Node(2);
@@ -56,9 +58,29 @@ public class BinarySearchTree{
         }
     }
 
+    //size() -> return the number of nodes in the BST
+    public int size(Node root){
+        if(root == null){
+            return 0;
+        }
+        int count = 1;
+        Node current = root;
+        if(current.right != null){
+            count += this.size(current.right);
+        }
+        if(current.left != null){
+            count += this.size(current.left);
+        }
+        return count;
+    }
+
+
+
+
     public static void main(String[] args){
         BinarySearchTree bst = new BinarySearchTree(null); 
         bst.build123();
         bst.printTree(bst.root);
+        System.out.println("Size of the current binary tree is: ->"+bst.size(bst.root));
     }
 }
