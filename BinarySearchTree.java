f02f6e802a6770c2367a3cb982795f4c52243903
lakshmi2812 <laksmi.nishthala@gmail.com>
2019-09-09 19:10:07 -0700

    BST first try

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,11 +1,59 @@
 public class BinarySearchTree{
+    Node root;
+
     private static class Node{
         int data;
         Node left;
         Node right;
-        private Node(Node left, Node right){
-            this.left = left;
-            this.right = right;
+        //constructor for a Node in the BST
+        private Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
         }
+    }
+
+    //constructor for a BST
+    public BinarySearchTree(Node root){
+        this.root = root;
+    }
+
+    //Basic Utility Functions
+    public void printTree(Node root){
+        if(root == null){
+            return;
+        }
+        Node current = this.root;
+        System.out.println(current.data+"->");
+        if(current.left != null){
+            this.printTree(current.left);
+        }
+        if(current.right != null){
+            this.printTree(current.right);
+        }
+    }
+    
+    
+    //Binary Tree Problems
+    public void build123(){
+        if(this.root == null){
+            this.root = new Node(2);
+        }else{
+            return;
+        }
+
+        if(this.root.left == null){
+            this.root.left = new Node(1);
+        }
+
+        if(this.root.right == null){
+            this.root.right = new Node(3);
+        }
+    }
+
+    public static void main(String[] args){
+        BinarySearchTree bst = new BinarySearchTree(null); 
+        bst.build123();
+        bst.printTree(bst.root);
     }
 }
