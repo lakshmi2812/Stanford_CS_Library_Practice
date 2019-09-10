e9c933eaad2520c3d8bb355ec824027cfb4dbf93
lakshmi2812 <laksmi.nishthala@gmail.com>
2019-09-09 17:16:00 -0700

    Started BinarySearchTree

---
+++ BinarySearchTree.java
@@ -0,0 +1,11 @@
+public class BinarySearchTree{
+    private static class Node{
+        int data;
+        Node left;
+        Node right;
+        private Node(Node left, Node right){
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
