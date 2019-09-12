e715552fbf9fbb984834ba316ae2bffdc22cbea7
lakshmi2812 <laksmi.nishthala@gmail.com>
2019-09-12 16:00:26 -0700

    Mirror works and just attempted hasPathSum

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -21,6 +21,8 @@ public class BinarySearchTree{
     }
 
     //Basic Utility Functions
+
+    //printTree() -> prints nodes in the tree by successively printing each level of the tree
     public void printTree(Node root){
         if(root == null){
             return;
@@ -39,11 +41,53 @@ public class BinarySearchTree{
             return;
         }
     }
+
+    //insert() -> insert a new Node with the given data into the BST
+    public void insert(int data){
+        // Node newNode = new Node(data);
+        // if(root == null){
+        //     root = newNode;
+        //     return;
+        // }
+        // Node current = root;
+        // if(data <= current.data){
+        //     insert(current.left, data);
+        // }else{
+        //     insert(current.right, data);
+        // }
+        Node newNode = new Node(data);
+        if(this.root == null){
+            this.root = newNode;
+        }
+
+        Node current = this.root;
+        while(current != null){
+            if(data <= current.data){
+                if(current.left == null){
+                    current.left = newNode;
+                    return;
+                }else{
+                    current = current.left;
+                }
+            }
+            if(data > current.data){
+                if(current.right == null){
+                    current.right = newNode;
+                    return;
+                }else{
+                    current = current.right;
+                }
+            }
+        }
+        current = new Node(data);
+    }
+
+    //lookup() -> look for a node containing the given data in the BST. Return true if a node with the given data exists and false otherwise
     
     
     //********/ Binary Tree Problems /**********/
 
-    //build123() -> Build a linkedlist with nodes 1,2,3
+    //1.build123() -> Build a linkedlist with nodes 1,2,3
     public void build123(){
         if(this.root == null){
             this.root = new Node(2);
@@ -60,7 +104,7 @@ public class BinarySearchTree{
         }
     }
 
-    //size() -> return the number of nodes in the BST
+    //2.size() -> return the number of nodes in the BST
     public int size(Node root){
         if(root == null){
             return 0;
@@ -76,7 +120,7 @@ public class BinarySearchTree{
         return count;
     }
 
-    //maxDepth() -> return the maximum depth(#oflevels) of the BST as an integer
+    //3.maxDepth() -> return the maximum depth(#oflevels) of the BST as an integer
     public int maxDepth(Node root){
         if(this.root == null){
             return 0;
@@ -89,7 +133,7 @@ public class BinarySearchTree{
         return depth;
     }
 
-    //minValue() -> return the minimum value present in the BST
+    //4.minValue() -> return the minimum value present in the BST
     public int minValue(Node root){
         if(root == null){
             return 0;
@@ -103,6 +147,99 @@ public class BinarySearchTree{
         return current.data;
     }
 
+    //printTreeInorder() is called printTree() in the pdf. This method prints the node sof the BST in the increasing order
+    //left,node and right nodes in that order
+    public void printTreeInorder(Node root){
+        if(root == null){
+            return;
+        }
+        Node current = root;
+        if(current.left != null){
+            printTreeInorder(current.left);
+        }
+        System.out.println(" "+current.data);
+        if(current.right != null){
+            printTreeInorder(current.right);
+        }
+    }
+
+    //printPostorder() -> prints the values of the left and right nodes of a node before printing its own value
+    public void printPostorder(Node root){
+        if(root == null){
+            return;
+        }
+        Node current = root;
+        if(current.left != null){
+            printTreeInorder(current.left);
+        }
+        if(current.right != null){
+            printTreeInorder(current.right);
+        }
+        System.out.println(" "+current.data);
+    }
+
+    //hasPathSum() -> if there is a path in the BST with the given sum, return true, else return false
+    public boolean hasPathSum(int inputSum, Node root){
+        if(root == null){
+            return false;
+        }
+
+        Node current = root;
+
+        // while(current.left != null || current.right != null){
+        //     if(current.left != null){
+
+        //     }
+        // }
+
+        //*******************/
+
+        int sum = current.data;
+        while(current.left != null || current.right != null){
+            System.out.println("SUM for each iteration: "+sum);
+            while(current.left != null){
+                current = current.left;
+                sum += current.data;
+            }
+            current = root;
+            sum = current.data;
+            if(sum == inputSum){
+                System.out.println("SUM before return in while: "+sum);
+                return true;
+            }
+            while(current.right != null){
+                current = current.right;
+                sum += current.data;
+            }
+        }
+        System.out.println("SUM outside while: "+sum);
+        if(sum == inputSum){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //mirror() -> create a mirror image of the current tree(by swapping the left and right nodes for each node)
+    public void mirror(Node root){
+        if(root == null){
+            return;
+        }
+
+        Node current = root;
+        Node temp = current.left;
+        current.left = current.right;
+        current.right = temp;
+        if(current.left != null){
+            mirror(current.left);
+        }
+        if(current.right != null){
+            mirror(current.right);
+        }
+    }
+
+
+
 
 
 
@@ -113,5 +250,21 @@ public class BinarySearchTree{
         System.out.println("Size of the current binary tree is: ->"+bst.size(bst.root));
         System.out.println("Depth of the current binary tree is: ->"+bst.maxDepth(bst.root));
         System.out.println("The minimum value of the current binary tree is: ->"+bst.minValue(bst.root));
+        bst.insert(5);
+        System.out.println("Inserting node 5 into BST: ->");
+        System.out.println("Size of the current binary tree is: ->"+bst.size(bst.root));
+        //bst.printTree(bst.root);
+        bst.printTreeInorder(bst.root);
+        System.out.println("Post order traversal of the tree: ");
+        bst.printPostorder(bst.root);
+        bst.insert(4);
+        bst.insert(9);
+        bst.insert(11);
+        System.out.println("Inorder traversal: ");
+        bst.printTreeInorder(bst.root);
+        System.out.println(bst.hasPathSum(30, bst.root));
+        bst.mirror(bst.root);
+        System.out.println("BST after mirror method: ");
+        bst.printTreeInorder(bst.root);
     }
 }
